9e0df613996278b56c3ee169cb8311581a0a37f8
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-06-22 18:07:02 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -41,7 +41,39 @@ module.exports = function (app, db, pgp) {
       });
   });
 
-
+app.get("/api/getJourneyByID", function (req, res) {
+    //res.render("index.ejs"); // load the index.ejs file
+  const results = [];
+    var query = "SELECT * FROM orkestra_Journeys";
+    db.query(query, true)
+      .then(function (data) {
+        return res.json(data);
+      })
+      .catch(function (err) {
+        console.log("ERROR:", err); // print the error;
+        return res.status(400).json({ success: false, error: err });
+      })
+   .finally(function () {
+        pgp.end(); // for immediate app exit, closing the connection pool.
+      });
+  });
+  
+  app.get("/api/getTemplateByID", function (req, res) {
+    //res.render("index.ejs"); // load the index.ejs file
+  const results = [];
+    var query = "SELECT * FROM orkestra_Templates";
+    db.query(query, true)
+      .then(function (data) {
+        return res.json(data);
+      })
+      .catch(function (err) {
+        console.log("ERROR:", err); // print the error;
+        return res.status(400).json({ success: false, error: err });
+      })
+   .finally(function () {
+        pgp.end(); // for immediate app exit, closing the connection pool.
+      });
+  });
   
 
 };
