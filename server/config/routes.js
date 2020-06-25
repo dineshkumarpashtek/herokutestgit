55821d276b04b6fb197c6997c18f900783a0cf01
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-06-25 18:59:53 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -14,7 +14,7 @@ module.exports = function (app, db, pgp) {
 
 app.get("/api/getJourneyByKey", function (req, res) {
     //res.render("index.ejs"); // load the index.ejs file
-  const {Journeykey}=req.body;
+  const { Journeykey }=req.body;
   //const {Journeykey}='ixn-created-Meeting-Executed-api';
   console.log('Journeykey:'+Journeykey);
   const results = [];
