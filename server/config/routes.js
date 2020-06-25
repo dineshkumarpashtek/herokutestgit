6e1401b75626e99901f863c93b6e6b232056f354
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-06-25 19:56:11 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -10,7 +10,21 @@ module.exports = function (app, db, pgp) {
   // =====================================
  
   
-  
+  app.get("/api/getJourneyByID", function (req, res) {
+    //res.render("index.ejs"); // load the index.ejs file
+    var query = "SELECT * FROM journey";
+    db.query(query, true)
+      .then(function (data) {
+        return res.json(data);
+      })
+      .catch(function (err) {
+        console.log("ERROR:", err); // print the error;
+        return res.status(400).json({ success: false, error: err });
+      })
+   .finally(function () {
+        pgp.end(); // for immediate app exit, closing the connection pool.
+      });
+  });
 
 app.get("/api/getJourneyByKey", function (req, res) {
     //res.render("index.ejs"); // load the index.ejs file
