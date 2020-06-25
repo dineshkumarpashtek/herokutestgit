068f8514123c7021a5f2f4526905394271c0fa51
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-06-25 16:30:15 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -18,7 +18,7 @@ app.get("/api/getJourneyByKey", function (req, res) {
   const {Journeykey}='ixn-created-Meeting-Executed-api';
   
   const results = [];
-    var query = "SELECT * FROM journey where Journey_key='"+req.Journeykey+"'";
+    var query = "SELECT * FROM journey where Journey_key='"+req.body.Journeykey+"'";
     db.query(query, true)
       .then(function (data) {
         return res.json(data);
