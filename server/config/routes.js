d6d1216b0c527000d9c633a4bf02258981cac057
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-07-03 12:53:47 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -193,7 +193,7 @@ app.post("/api/getJourneyByKey", function (req, res) {
    //var journey_key ='ixn-created-Meeting-Executed-api';
    var customer_id;
    var creative_id;
-   var customer_creative_id;
+   var customer_creative_id='';
    var journey_id;
    console.log('customer_key:'+customer_key);
    console.log('Template_key:'+Template_key);
@@ -224,7 +224,7 @@ app.post("/api/getJourneyByKey", function (req, res) {
       console.log('customer_creative_id:'+customer_creative_id);
          }
         }
-      if(customer_creative_id != creative_id){
+      if(customer_creative_id != creative_id && customer_creative_id != ''){
         console.log('inside if condtion');
       var insertQuery =
       "INSERT INTO customer_creative (customerid, journeyid, creativeid) VALUES ('" +
