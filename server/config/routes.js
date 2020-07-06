a4dd945fb34774e1e7ae11a7e84c42e19ff22e2e
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-07-06 16:11:00 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -259,7 +259,7 @@ app.post("/api/getJourneyByKey", function (req, res) {
    
    });
  
-   app.get("/api/getExtensionKey", function (req, res) {
+   app.post("/api/getExtensionKey", function (req, res) {
    const { Extension_Key } = req.body;
   console.log('Extension_Key:'+Extension_Key);  
   const results = [];
