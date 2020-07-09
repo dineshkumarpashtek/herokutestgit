ab774a4ba1b5c2d3a732ee7da67ae0f0b570d150
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-07-09 10:17:40 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,15 +1,4 @@
-//var jsforce = require("jsforce");
 
-// Salesforce OAuth2 client information
-/*var conn = new jsforce.Connection({
-  oauth2: {
-  //   you can change loginUrl to connect to sandbox or prerelease env.
-    loginUrl: "https://login.salesforce.com",
-    clientId: process.env.Consumer_Key,
-    clientSecret: process.env.Consumer_Secret,
-    redirectUri: process.env.Callback_URL,
-  },
-});*/
 
 //all the routes for our application
 module.exports = function (app, db, pgp) {
