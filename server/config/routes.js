ce0578184af4b6dfa8cc310108a5f8d44ca3ec7c
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-07-13 12:01:41 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -67,22 +67,6 @@ app.post("/api/getJourneyByKey", function (req, res) {
   });
 
   
-  app.get("/api/getTemplateByKey", function (req, res) {
-    //res.render("index.ejs"); // load the index.ejs file
-  const results = [];
-    var query = "SELECT * FROM orkestra_Templates";
-    db.query(query, true)
-      .then(function (data) {
-        return res.json(data);
-      })
-      .catch(function (err) {
-        console.log("ERROR:", err); // print the error;
-        return res.status(400).json({ success: false, error: err });
-      })
-   .finally(function () {
-        pgp.end(); // for immediate app exit, closing the connection pool.
-      });
-  });
   
  app.post("/api/updatecustomerjourney", function (req, res) {
     //res.render("index.ejs"); // load the index.ejs file
