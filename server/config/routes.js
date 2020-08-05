ebe30a57401781333a4978dc025b2a31a8761192
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2020-08-05 14:07:23 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -48,6 +48,32 @@ app.post("/api/getJourneyByKey", function (req, res) {
         pgp.end(); // for immediate app exit, closing the connection pool.
       });
   });
+  
+  
+  app.post("/api/getCustomerJourneyByKey", function (req, res) {
+    //res.render("index.ejs"); // load the index.ejs file
+  //var a=req.body;
+  const { journey_key } = req.body;
+  //const {Journeykey}='ixn-created-Meeting-Executed-api';
+  console.log('req.body:'+req.body);
+  console.log('journey_key:'+journey_key);
+  const results = [];
+    var query = "select * from customer_journey \
+INNER JOIN journey \
+ON customer_journey.journey_id = journey.id \ 
+where journey_key = '" + journey_key + "'";
+    db.query(query, true)
+      .then(function (data) {
+        return res.json(data);
+      })
+      .catch(function (err) {
+        console.log("ERROR:", err); // print the error;
+        return res.status(400).json({ success: false, error: err });
+      })
+   .finally(function () {
+        pgp.end(); // for immediate app exit, closing the connection pool.
+      });
+  });
 
   app.post("/api/GetCreativeByKey", function (req, res) {
     //res.render("index.ejs"); // load the index.ejs file
