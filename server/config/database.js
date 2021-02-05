a040b2385875301e3056310f8b769a74677c93a9
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2021-02-05 14:27:31 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,19 @@
-var pgp = require("./pgpromise.js");
+var pgp = require("./pgpromise.js")(options);
 // See also: https://github.com/vitaly-t/pg-promise#initialization-options
+var options = {
+    error: function (error, e) {
+        if (e.cn) {
+            // A connection-related error;
+            console.log("CN:", e.cn);
+            console.log("EVENT:", error.message);
+        }
+    }
+};
+const db = pgp('postgresql://xikaepnupjihny:ed8939f2a3555e38e3ff018e0adb5885463936afffd70c501f6b1d5fcefdf7a3@ec2-3-218-75-21.compute-1.amazonaws.com:5432/d8muah1afp73t');
+
 
 // Database connection details;
-var cn = {
+/*var cn = {
   host: "ec2-3-218-75-21.compute-1.amazonaws.com", // 'localhost' is the default;
   port: 5432, // 5432 is the default;
   database: "d8muah1afp73t",
@@ -12,8 +23,9 @@ var cn = {
 };
 // You can check for all default values in:
 // https://github.com/brianc/node-postgres/blob/master/lib/defaults.js
-var db = pgp(cn) // database instance;
-.then(obj => {
+var db = pgp(cn); // database instance; */
+db.connect()
+    .then(obj => {
         obj.done(); // success, release the connection;
     })
     .catch(error => {
