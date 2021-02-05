c61f92f2925a7c33d955c353b0ce896e75653ccc
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2021-02-05 16:32:49 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -5,14 +5,16 @@ var pgp = require("./pgpromise.js");
 
 
 // Database connection details;
-var cn = {
+/*var cn = {
   host: "ec2-3-218-75-21.compute-1.amazonaws.com", // 'localhost' is the default;
   port: 5432, // 5432 is the default;
   database: "d8muah1afp73t",
   user: "xikaepnupjihny",
   password: "ed8939f2a3555e38e3ff018e0adb5885463936afffd70c501f6b1d5fcefdf7a3",
   ssl: true
-};
+}; */
+console.log('process.env.DATABASE_URL:'+process.env.DATABASE_URL);
+var cn=process.env.DATABASE_URL;
 // You can check for all default values in:
 // https://github.com/brianc/node-postgres/blob/master/lib/defaults.js
 var db = pgp(cn); // database instance; 
