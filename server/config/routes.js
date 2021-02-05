efce649d6155d241ce5f388b873e759205471036
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2021-02-05 13:23:20 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,7 +8,7 @@ module.exports = function (app, db, pgp) {
  
   app.post("/authentication",(req,res) =>{
   
-    var query = "select * from dataextension;";
+    var query = "select * from dataextension";
      db.query(query, true)
       .then(function (data) {
         return res.json(data);
