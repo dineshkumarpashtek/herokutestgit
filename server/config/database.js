50634ad14177de2c333b455386961e9b54a1390e
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2021-02-08 17:31:52 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -33,5 +33,7 @@ const client = new Client({
 
 });
 
-client.connect();
+client.connect()
+.then(() => console.log('connected'))
+  .catch(err => console.error('connection error', err.stack))
 module.exports = client;
