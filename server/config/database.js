6742eb867c610677801c1234b445af68c15d31c0
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2021-02-09 13:37:08 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -22,7 +22,7 @@ var db = pgp(cn); // database instance;
 
 module.exports = db; */
 
-const { Client } = require('pg');
+/*const { Client } = require('pg');
 const connectionString = 'postgres://xikaepnupjihny:ed8939f2a3555e38e3ff018e0adb5885463936afffd70c501f6b1d5fcefdf7a3@ec2-3-218-75-21.compute-1.amazonaws.com:5432/d8muah1afp73t';
 
 const client = new Client({
@@ -39,6 +39,15 @@ client.connect(err => {
   } else {
     console.log('connected');
   }
-  })
+  }) */
+const { Pool, Client } = require('pg')
+//const connectionString = 'postgres://xikaepnupjihny:ed8939f2a3555e38e3ff018e0adb5885463936afffd70c501f6b1d5fcefdf7a3@ec2-3-218-75-21.compute-1.amazonaws.com:5432/d8muah1afp73t'
+console.log('process.env.DATABASE_URL:'+process.env.DATABASE_URL);
+const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+      }
+})
 
-module.exports = client;
+module.exports = pool;
