b0d46d8ed17601f96d7ab31202795daff85afec9
dineshkumarpashtek <63998134+dineshkumarpashtek@users.noreply.github.com>
2021-02-09 13:21:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var session      = require('express-session');
 
 var pgp = require('./server/config/pgpromise.js');  
 var db= require('./server/config/database.js') 
-console.log('db:'+JSON.stringify(db));
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
